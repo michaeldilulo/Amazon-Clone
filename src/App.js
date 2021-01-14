37fdfd7b6a8688fe0870784fadcccdbd807f4dfe
michael dilulo <snoleopard01001101@gmail.com>
2021-01-13 17:40:27 -0800

    Route setup with checkout, login and home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom"
 
 function App() {
   return (
+    <Router>
     <div className="app">
-      <h1>Amazon Clone</h1>
+        <Switch>
+          <Route path="/checkout">
+          <h1>Checkout</h1>
+          </Route>
+          <Router path="/login">
+          <h1>Login</h1>
+          </Router>
+          <Route path="/">
+            <h1>Home Page</h1>
+          </Route>
+     </Switch>
     </div>
+    </Router>
   );
 }
 
