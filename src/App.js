d4552aa9eb9fa04a28f5630e01c33dd31a224fe1
michael dilulo <snoleopard01001101@gmail.com>
2021-01-17 10:03:56 -0800

    Header show on checkout page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <div className="app">
         <Switch>
           <Route path="/checkout">
+            <Header />
           <h1>Checkout</h1>
           </Route>
           <Route path="/login">
