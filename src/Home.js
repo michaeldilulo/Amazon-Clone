9be06a926db1ac859c79274ad2e40158b385a6d2
michael dilulo <snoleopard01001101@gmail.com>
2021-01-18 08:34:13 -0800

    Home Row with 2 products in row display

--- src/Home.js
+++ src/Home.js
@@ -7,13 +7,22 @@ function Home() {
         <div className="home">
             <img className="home__image" src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" />
             
+            <div className="home__row">
             <Product
                 id="12345"
                 title="The Lean Startup"
                 price={11.96}
                 rating={5}
                 image="https://thumbs-prod.si-cdn.com/ufPRE9RHUDHqQdOsLvYHhJAxy1k=/fit-in/1600x0/https://public-media.si-cdn.com/filer/91/91/91910c23-cae4-46f8-b7c9-e2b22b8c1710/lostbook.jpg"
+                />
+                            <Product
+                id="12345"
+                title="The Lean Startup"
+                price={11.96}
+                rating={5}
+                image="https://thumbs-prod.si-cdn.com/ufPRE9RHUDHqQdOsLvYHhJAxy1k=/fit-in/1600x0/https://public-media.si-cdn.com/filer/91/91/91910c23-cae4-46f8-b7c9-e2b22b8c1710/lostbook.jpg"
             />
+            </div>
         </div>
     )               
 }
