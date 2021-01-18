cc1e9bc7581baf08906f8ed2615fc644340b552d
michael dilulo <snoleopard01001101@gmail.com>
2021-01-18 08:02:04 -0800

    Displays Star rating with StarIcon from mat-ui

--- src/Product.js
+++ src/Product.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./Product.css"
+import StarIcon from "@material-ui/icons/Star"
 
 function Product({id, title, price, rating, image}) {
     return (
@@ -9,6 +10,15 @@ function Product({id, title, price, rating, image}) {
                 <small>$</small>
                 <strong>{price}</strong>
             </p>
+            <div className="product__rating">
+                {
+                    Array(rating)
+                        .fill()
+                        .map((_) => (
+                            <p><StarIcon /></p>
+                        ))
+                }
+            </div>
         </div>
     )
 }
