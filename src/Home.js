6c765b360a721d12fbe23f02d45cf9ce395ab4b9
michael dilulo <snoleopard01001101@gmail.com>
2021-01-18 07:55:53 -0800

    Product appears on page

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import "./Home.css"
+import Product from "./Product"
 
 function Home() {
     return (
         <div className="home">
             <img className="home__image" src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" />
-            {/* Product id, title, price, rating, image */}
-            {/* Product */}
+            
+            <Product
+                id="12345"
+                title="The Lean Startup"
+                price={11.96}
+                rating={5}
+                image="https://thumbs-prod.si-cdn.com/ufPRE9RHUDHqQdOsLvYHhJAxy1k=/fit-in/1600x0/https://public-media.si-cdn.com/filer/91/91/91910c23-cae4-46f8-b7c9-e2b22b8c1710/lostbook.jpg"
+            />
         </div>
-    )
+    )               
 }
 
 export default Home
