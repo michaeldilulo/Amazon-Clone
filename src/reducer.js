44ec749e6288ab1b07b8a057fbde588a2753f002
michael dilulo <snoleopard01001101@gmail.com>
2021-01-20 10:55:08 -0800

    Switch statement setup for add / remove basket

--- src/reducer.js
+++ src/reducer.js
@@ -2,3 +2,15 @@ export const initialState = {
     basket: [],
 }
 
+function reducer(state, action) {
+    switch (action.type) {
+        case 'ADD_TO_BASKET':
+            //logic for adding item to basket
+            break;
+        case 'REMOVE_FROM_BASKET':
+            //logic for removing item from basket
+            break;
+        default:
+            return state;
+    }
+}
