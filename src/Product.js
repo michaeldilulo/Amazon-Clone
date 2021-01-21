26a91401af1e6e5fccecd71d15c76d062b709676
michael dilulo <snoleopard01001101@gmail.com>
2021-01-20 23:28:20 -0800

    addToBasket function item / type

--- src/Product.js
+++ src/Product.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import "./Product.css"
 import StarIcon from "@material-ui/icons/Star"
+import { useStateValue } from './StateProvider'
 
 function Product({ id, title, price, rating, image }) {
+    const [{basket}, dispatch] = useStateValue()
     
     const addToBasket = () => {
-        
+        dispatch({
+            type: 'ADD_TO_BASKET',
+            item: {
+                id: id,
+                title: title,
+                price: price,
+                rating: rating,
+                image: image
+            }
+        })
     }
 
     return (
