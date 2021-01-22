68e3c2414c648e0c80611ff76b0417cb47014f93
michael dilulo <snoleopard01001101@gmail.com>
2021-01-22 10:41:17 -0800

    Item number displays when add to basket

--- src/reducer.js
+++ src/reducer.js
@@ -8,13 +8,13 @@ const reducer = (state, action) => {
     switch (action.type) {
         case 'ADD_TO_BASKET':
             //logic for adding item to basket
-            return {state}
-        case 'REMOVE_FROM_BASKET':
-            //logic for removing item from basket
             return {
                 ...state,
                 basket: [...state.basket, action.item]
             }
+        case 'REMOVE_FROM_BASKET':
+            //logic for removing item from basket
+            return {state}
         default:
             return state;
     }
