b149cf8dbd809a95f4b64f4687b0330be4bf8969
michael dilulo <snoleopard01001101@gmail.com>
2021-01-23 10:36:08 -0800

    Items populate on Checkout Page

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {useStateValue} from "./StateProvider"
 import "./Checkout.css"
+import CheckoutProduct from "./CheckoutProduct"
 
 function Checkout() {
     const [{ basket }] = useStateValue();
@@ -11,10 +12,21 @@ function Checkout() {
             {basket?.length === 0 ? (
                 <div>
                     <h2>Your Shopping Basket Is Empty</h2>
+                    <p>You have no items in your basket. To buy one or more items, click "Add to basket" next to the item.</p>
                 </div>
             ) : (
             <div>
-                <h2>Your Shopping Basket</h2>
+                        <h2 className="checkout__title">Your Shopping Basket</h2>
+                        {basket.map(item => (
+                            <CheckoutProduct
+                                item={item.id}
+                                title={item.title}
+                                image={item.image}
+                                price={item.price}
+                                rating={item.rating}
+                            />
+
+                        ))}
             </div>
             )}
         </div>
---
+++ src/CheckoutProduct.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import "./CheckoutProduct.css"
+import StarIcon from "@material-ui/icons/Star"
+
+function CheckoutProduct({id, title, price, rating, image}) {
+    return (
+        <div className="checkoutProduct">
+            <img src={image} alt="" />
+
+            <div className="checkoutProduct__info">
+                <p className="checkoutProduct__title">{title}</p>
+                <p className="checkoutProduct__price">
+                    <small>$</small>
+                    <strong>{price}</strong>
+                </p>
+                <div className="checkoutProduct__rating">
+                    {Array(rating)
+                        .fill()
+                        .map((_, i) => (
+                            <p><StarIcon /></p>
+                        ))
+                    }
+                </div>
+                <button>Remove From Basket</button>
+            </div>
+        </div>
+    )
+}
+
+export default CheckoutProduct
