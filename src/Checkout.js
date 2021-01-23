5f6a8d912e426f18f3f5e27ba288ea744c559757
michael dilulo <snoleopard01001101@gmail.com>
2021-01-23 10:24:59 -0800

    Optional with Items and Empty Shopping Basket

--- src/Checkout.js
+++ src/Checkout.js
@@ -6,7 +6,17 @@ function Checkout() {
     const [{ basket }] = useStateValue();
     return (
         <div className="checkout">
-            <img className="checkout__ad" src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="" />
+            <img className="checkout__ad" src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt=""
+            />
+            {basket?.length === 0 ? (
+                <div>
+                    <h2>Your Shopping Basket Is Empty</h2>
+                </div>
+            ) : (
+            <div>
+                <h2>Your Shopping Basket</h2>
+            </div>
+            )}
         </div>
     )
 }
