05d4500a72bca5004e35ceaf6774a5c809c102d8
michael dilulo <snoleopard01001101@gmail.com>
2021-01-24 11:17:36 -0800

    Total displays on checkout page

--- src/reducer.js
+++ src/reducer.js
@@ -3,6 +3,10 @@ export const initialState = {
     user: null
 }
 
+// main purpose of a selector is you can call it and it will give you the total
+export const getBasketTotal = (basket) => 
+    basket?.reduce((amount, item) => item.price + amount, 0)
+
 const reducer = (state, action) => {
 
     switch (action.type) {
